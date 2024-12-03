b894bd8f1d79e7ccc9085fd65da7d218c3eb25b5
baqer1413 <87332748+baqer1413@users.noreply.github.com>
2024-12-03 22:46:00 +0300

    Delete site-bundle.72e5364d6a59fa8b809228ec33508ee5.js

--- site-bundle.72e5364d6a59fa8b809228ec33508ee5.js
+++
@@ -1,5 +0,0 @@
-/*! For license information please see site-bundle.js.LICENSE.txt */
-!function(t,e){if("object"==typeof exports&&"object"==typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var r=e();for(var n in r)("object"==typeof exports?exports:t)[n]=r[n]}}
-  ,
-   
-
